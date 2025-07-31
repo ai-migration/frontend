ef6750abce81f712a18584bb5f95daf7b53cb956
hyunseong <dnclgk9@naver.com>
2025-07-31 15:43:52 +0900

    0731_홈페이지 하단 UI 변경(실무 코치님 피드백 반영)

--- src/components/EgovFooter.jsx
+++ src/components/EgovFooter.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-import logoFooterImg from "/assets/images/logo_footer_aivle.png";
+import logoFooterImg from "/assets/images/logo_footer_ai.png";
 import logoFooterImgMobile from "/assets/images/logo_footer_m.png";
 import bannerImg_01 from "/assets/images/banner_w_01.png";
 import bannerImgMobile_01 from "/assets/images/banner_m_01.png";
@@ -12,33 +12,75 @@ function EgovFooter() {
     <div className="footer">
       <div className="inner">
         <h1>
-          <Link to="">
-            <img className="w" src={logoFooterImg} alt="" />
-            <img className="m" src={logoFooterImgMobile} alt="" />
+          <Link to="/">
+            <img className="w" src={logoFooterImg} alt="AIVLE" />
+            <img className="m" src={logoFooterImgMobile} alt="AIVLE Mobile" />
           </Link>
         </h1>
+
         <div className="info">
           <p>
-            GitHub : https://github.com/leeyumin-dev/ai_migration{" "}
-            <span className="m_hide">|</span>
-            <br className="m_show" /> 대표전화 : 010-4463-5077
+            GitHub :{" "}
+            <a
+              href="https://github.com/leeyumin-dev/ai_migration"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              바로가기
+            </a>{" "}
+            <span className="m_hide"> | </span>
+            <br className="m_show" />
+            대표전화 : 010-4463-5077
             <br />
-            Notion : https://www.notion.so/AI-Code-Migration-2297bde02a66809da989c76132e4cdc5?p=23e7bde02a668016996ac7a4802ebcef&pm=s | 교육문의 : 010-4463-5077
+            Notion :{" "}
+            <a
+              href="https://www.notion.so/AI-Code-Migration-2297bde02a66809da989c76132e4cdc5?p=23e7bde02a668016996ac7a4802ebcef&pm=s"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              바로가기
+            </a>{" "}
+             |  교육문의 : 010-4463-5077
           </p>
+
+          <p className="policy-links">
+            <strong>
+              <a
+                href="https://aivle.edu.kt.co.kr/home/main/privateMain"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                AI CODE MIGRATION 개인정보 처리방침
+              </a>
+            </strong>{" "}
+            |{" "}
+            <a
+              href="https://aivle.edu.kt.co.kr/home/main/policySvcMain"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              이용약관
+            </a>
+          </p>
+
           <p className="copy">
-            Copyright © 2025 KT AIVLESCHOOL BIG PROJECT GROUP 10
-            Reserved.
+
+            <br />
+            Copyright © 2025 KT AIVLE SCHOOL BIG PROJECT GROUP 10. All rights reserved.
           </p>
         </div>
+
         <div className="right_col">
-          <Link to="">
-            <img className="w" src={bannerImg_01} alt="" />
-            <img className="m" src={bannerImgMobile_01} alt="" />
-          </Link>
-          <Link to="">
-            <img className="w" src={bannerImg_02} alt="" />
-            <img className="m" src={bannerImgMobile_02} alt="" />
-          </Link>
+          <a href="https://www.mois.go.kr" target="_blank" rel="noopener noreferrer">
+            <img className="w" src={bannerImg_01} alt="행정안전부" />
+            <img className="m" src={bannerImgMobile_01} alt="행정안전부 모바일" />
+          </a>
+          <a href="https://www.nia.or.kr" target="_blank" rel="noopener noreferrer">
+            <img className="w" src={bannerImg_02} alt="NIA" />
+            <img className="m" src={bannerImgMobile_02} alt="NIA 모바일" />
+          </a>
         </div>
       </div>
     </div>
